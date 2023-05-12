8ec5550ce3150fb715e72949df03aaa15f456dc6
texasnk <davirodriguesnk@gmail.com>
2023-05-12 15:38:55 -0300

    Create models

---
+++ ReviewAPI/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ ReviewAPI/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewAPI.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
