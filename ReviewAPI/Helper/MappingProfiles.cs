e10fe282daca5b46e422240f3b62430138ac89ac
texasnk <davirodriguesnk@gmail.com>
2023-05-15 15:27:52 -0300

    Extend IPokemonRepository, implement and integrate AutoMapper

---
+++ ReviewAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReviewAPI.Dto;
+using ReviewAPI.Models;
+
+namespace ReviewAPI.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
--- ReviewAPI/Repository/PokemonRepository.cs
+++ ReviewAPI/Repository/PokemonRepository.cs
@@ -7,7 +7,6 @@ namespace ReviewAPI.Repository
     public class PokemonRepository : IPokemonRepository
     {
         private readonly DataContext _context;
-
         public PokemonRepository(DataContext context)
         {
             _context = context;
@@ -16,6 +15,30 @@ namespace ReviewAPI.Repository
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p=>p.Id).ToList();
+        }
+ 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p=>p.Name==name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            if (review.Count() <= 0) return 0;
+
+            return((decimal)review.Sum(r => r.Rating)/review.Count());
+        }
+
+   
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p=>p.Id==pokeId);
         }
     }
 }
